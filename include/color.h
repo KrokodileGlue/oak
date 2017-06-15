7671d50dd783562048970f77756857dba8457158
KrokodileGlue <taylorwest111@gmail.com>
2017-06-15 15:27:55 -0700

    better colors

--- include/color.h
+++ include/color.h
@@ -1,10 +1,10 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#define ERROR_MSG_COLOR "\x1B[31m"
-#define ERROR_HIGHLIGHT_COLOR "\x1B[33m"
-#define ERROR_NOTE_COLOR "\x1B[34m"
-#define ERROR_LOCATION_COLOR "\x1B[37m"
+#define ERROR_MSG_COLOR "\x1B[1m\x1B[31m"
+#define ERROR_HIGHLIGHT_COLOR "\x1B[1m\x1B[33m"
+#define ERROR_NOTE_COLOR "\x1B[1m\x1B[34m"
+#define ERROR_LOCATION_COLOR "\x1B[1m\x1B[37m"
 #define RESET_COLOR "\x1B[0m"
 
 #endif
