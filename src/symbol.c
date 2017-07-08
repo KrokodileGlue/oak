9bc0902a706e5d3b8105f71c502bddae3fa21bdc
KrokodileGlue <taylorwest111@gmail.com>
2017-07-08 15:21:13 -0700

    move ast printer

---
+++ src/symbol.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+uint64_t hash(char *d, size_t len)
+{
+	uint64_t hash = 5381;
+
+	for (size_t i = 0; i < len; i++)
+		hash = ((hash << 5) + hash) + d[i];
+
+	return hash;
+}
